01d0ea58be6afdf5901ee4dbaf316f794f5ef61e
Aryan Gupta <ag1121428@gmail.com>
2025-08-29 03:19:01 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import { JWT_SECRET } from "../config.js"; // Import secret key
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
